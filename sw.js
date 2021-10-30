26d2edac83e11c0f9e32b4ff45170bd097fafb5c
Ramesh <92715951+Ramesh-lg@users.noreply.github.com>
2021-10-30 22:32:10 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,58 @@
-const CACHE_NAME = "static_cache"
- 
-const STATIC_ASSETS =[
-    '/index.html',
-    '/styles.css'
+var APP_PREFIX = 'ApplicationName_'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                       // Add URL you want to cache in this list.
+  '/https://ramesh-lg.github.io/Ramesh-portfolio/',                     // If you have separate JS/CSS files,
+  '/https://ramesh-lg.github.io/Ramesh-portfolio/index.html'            // add path to those files here
 ]
 
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
 
-async function preCache(){
-    const cache =await caches.open(CACHE_NAME)
-    return cache.addAll(STATIC_ASSETS)
-}
-
-self.addEventListener('install', event =>{
-    console.log("[SW] installed");
-    event.waitUntil(preCache())
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
 })
 
-self.addEventListener('activate', event =>{
-    console.log("[SW] activated");
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
 })
 
-async function fetchAssets(event){
-    try{
-        const response = await fetch(event.request)
-        return response
-    }catch(err){
-        const caches = await caches.open(CACHE_NAME)
-        return cache.match(event.request)
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
 
-    }
-}
-self.addEventListener('fetch', event =>{
-    console.log("[SW] fetched");
-    event.respondWith(fetchAssets(event))
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
 })
